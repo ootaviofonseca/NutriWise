65aae06f3da2a1d40b813fdb25dd7dd924dd5a01
GregoSX <guilhermegrego376@gmail.com>
2024-07-11 18:53:41 -0300

    feat: Tela inicial

--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from galeria.views import pageInicial
 
 urlpatterns = [
+    path('', pageInicial),
     path('admin/', admin.site.urls),
 ]
