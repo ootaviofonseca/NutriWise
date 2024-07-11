e6633c49fbc8e296fe4bfd3a1b01c02bfd547851
Thiago Odilon <thiago-almeida05@hotmail.com>
2024-07-11 14:57:52 -0300

    feat: adicionando model

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    senha = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11, primary_key=True)
+    
+    def __str__(self):
+        return self.nome
+
+class Administrador(models.Model):
+    nome = models.CharField(max_length=100)
+    senha = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nome
+
+class Refeicoes(models.Model):
+    alimento = models.CharField(max_length=100)
+    quantidade = models.FloatField()
+    horario = models.DateTimeField()
+    
+    def __str__(self):
+        return self.alimento
+
+class TabelaNutricional(models.Model):
+    usuarioReferencia = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+    usuarioPeso = models.FloatField()
+    gorduraCorporal = models.FloatField()
+    refeicoes = models.ManyToManyField(Refeicoes)
+    objetivo = models.CharField(max_length=200)
+    exames = models.TextField()
+    
+    def __str__(self):
+        return f"Tabela Nutricional de {self.usuarioReferencia.nome}"
